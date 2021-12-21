f36d0f737f12e2e8cf9badd0b1209d4d83100ba5
Hai Pham <yukihai1591@gmail.com>
2021-12-21 16:54:42 +0700

    Add timeout

--- frontend2/src/components/MainWindow.vue
+++ frontend2/src/components/MainWindow.vue
@@ -361,7 +361,7 @@ export default {
       };
       let result;
       await axios
-        .post(backend_url, data, {timeout: 100*1000})
+        .post(backend_url, data, {timeout: 100*1000, decompress: false})
         .then((data) => {
           const csv_data = data.data.csv_content;
           result = csv_data;
