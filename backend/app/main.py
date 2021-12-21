c256e66672b1479a81aab13dfbc9e4607d282e6e
Hai Pham <yukihai1591@gmail.com>
2021-12-21 16:29:50 +0700

    zip file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 import requests
+import ujson as json
+import gzip
+
 
 app= Flask(__name__)
 CORS(app)
+
+# @app.route('/getData', methods=['GET', 'POST'])
+# def index():
+#     data = request.get_json()
+#     print("DATA", data)
+#     exportLink = data['exportLink']
+#     req = requests.get(exportLink)
+#     url_content = req.content
+#     decode_data = url_content.decode('utf-8')
+#     result = {
+#         "csv_content": decode_data
+#     }
+#     return jsonify(result)
+
 
 @app.route('/getData', methods=['GET', 'POST'])
 def index():
@@ -13,9 +30,11 @@ def index():
     req = requests.get(exportLink)
     url_content = req.content
     decode_data = url_content.decode('utf-8')
-    result = {
-        "csv_content": decode_data
-    }
-    return jsonify(result)
 
+    compression_level = 9  # of 9 max
+    content = gzip.compress(url_content, compression_level)
+    response = make_response(content)
+    response.headers['Content-length'] = len(content)
+    response.headers['Content-Encoding'] = 'gzip'
+    return response
   
